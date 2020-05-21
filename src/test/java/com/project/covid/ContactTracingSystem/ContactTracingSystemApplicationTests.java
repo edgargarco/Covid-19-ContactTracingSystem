da289a61139c8e768166876448ee332304ec62f0
edgargarco <20161143@ce.pucmm.edu.do>
2020-05-20 21:19:04 -0400

    first commit

---
+++ src/test/java/com/project/covid/ContactTracingSystem/ContactTracingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.covid.ContactTracingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactTracingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
